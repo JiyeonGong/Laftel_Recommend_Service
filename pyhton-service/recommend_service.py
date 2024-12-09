555605c22a82a016ff52308ce883f3bf972719fc
JiyeonGong <gamja_da@naver.com>
2024-12-09 12:22:53 +0900

    service

--- pyhton-service/recommend_service.py
+++ pyhton-service/recommend_service.py
@@ -405,7 +405,7 @@ def get_combined_recommendations():
             chatbot_response = "추천할 수 있는 애니메이션이 없습니다. 다른 질문을 해보세요."
 
     #디버깅
-    except OpenAIError as e:
+    except openai.OpenAIError as e:
         print(f"OpenAI 호출 중 오류 발생: {e}")
         return jsonify({"error": "OpenAI API 호출 중 오류가 발생했습니다.", "details": str(e)}), 500
 
